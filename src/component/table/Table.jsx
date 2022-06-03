6b4e895bd3e0064b2fabbcef972e360a907bedee
Boucher Alexis <82830827+Alexlemars@users.noreply.github.com>
2022-06-03 17:17:57 +0200

    Added employee search function // Sort function in the employee table // Ajouts des employées dans le tableau via redux

--- src/component/table/Table.jsx
+++ src/component/table/Table.jsx
@@ -1,47 +1,100 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { useSelector } from 'react-redux';
+
 import Input from '../input/Input'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import Button from '../button/Button';
 
 
-
-
-
 export default function Table() {
+
+    const [search,setSearch]= useState("")
+    
+    const employees = useSelector(state => state.employees.employees)
+    const [dataArray,setDataArray]= useState([])
+
+    const [dataSorted, setDataSorted] = useState(false);
+    const [order, setOrder] = useState("ascending");
+
+    const sorting = (col) => {
+      if (order === "ascending") {
+        const sorted = [...dataArray].sort((a, b) =>
+          a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+        );
+        setDataArray(sorted);
+        setOrder("descending");
+        setDataSorted(true);
+      }
+      if (order === "descending") {
+        const sorted = [...dataArray].sort((a, b) =>
+          a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+        );
+        setDataArray(sorted);
+        setOrder("ascending");
+        setDataSorted(false);
+      }
+    };
+
+  
+
+
+    useEffect(()=>{
+        setDataArray(employees)
+    },[employees])
+    
+
 
 
     const icon = ()=>{
-        return <span className='icon-carret'><FontAwesomeIcon className='icon-carret_down' icon={faCaretDown}/></span>
+        return <span className='icon-carret-up'><FontAwesomeIcon className={dataSorted ? 'icon-carret_down' : "icon-carret-up" } icon={faCaretDown}/></span>
     }
 
-    const onClick = ()=>{
+    
 
-    }
+   
+     
+    
 
     const Users = () => {
-        return <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+        return dataArray.filter((element)=>(
+            element.firstName.toLowerCase().includes(search)||
+            element.lastName.toLowerCase().includes(search) ||
+            element.startDate.toLowerCase().includes(search) ||
+            element.department.toLowerCase().includes(search) ||
+            element.dateOfBirth.toLowerCase().includes(search) ||
+            element.street.toLowerCase().includes(search) ||
+            element.city.toLowerCase().includes(search) ||
+            element.state.toLowerCase().includes(search) ||
+            element.zipCode.toLowerCase().includes(search)
+
+
+        )).map(element =>(
+            
+            <tbody key={element.firstName}>
+            <tr>
+            <td>{element.firstName}</td>
+            <td>{element.lastName}</td>
+            <td>{element.startDate}</td>
+            <td>{element.department}</td>
+            <td>{element.dateOfBirth}</td>
+            <td>{element.street}</td>
+            <td>{element.city}</td>
+            <td>{element.state}</td>
+            <td>{element.zipCode}</td>
         </tr>
+        </tbody>      
+        )) 
+         
     }
-
-
-
 
 
   return (
     <div className='table-container'>
     <div className='table_flex'>
-        
     <div>
+        
     
         Show <select>
             <option>10</option>
@@ -50,28 +103,32 @@ export default function Table() {
             <option>100</option>
             </select> entries
     </div>
-    <Input className="table_flex_search" label="Search : "/>
+    
+    <Input value={search}  onChange={(e) => setSearch(e.target.value.toLowerCase())} className="table_flex_search" label="Search : "/>
     </div>
         <table className='table_style'>
             <thead>
                 <tr>
-                    <th onClick={onClick}>First Name {icon()}</th>
-                    <th>Last Name {icon()}</th>
-                    <th>Start Date {icon()}</th>
-                    <th>Departement {icon()}</th>
-                    <th>Date of Birth {icon()}</th>
-                    <th> Street {icon()}</th>
-                    <th>City {icon()}</th>
-                    <th>State {icon()}</th>
-                    <th>Zip Code {icon()}</th>
+                    <th onClick={()=> sorting("firstName")} >First Name {icon()}</th>
+                    <th onClick={()=> sorting("lastName")}>Last Name </th>
+                    <th onClick={()=> sorting("startDate")}>Start Date </th>
+                    <th onClick={()=> sorting("department")}>Departement </th>
+                    <th onClick={()=> sorting("dateOfBirth")}>Date of Birth </th>
+                    <th onClick={()=> sorting("street")}> Street </th>
+                    <th onClick={()=> sorting("city")}>City </th>
+                    <th onClick={()=> sorting("state")}>State </th>
+                    <th onClick={()=> sorting("zipCode")}>Zip Code</th>
                 </tr>
             </thead>
-            <tbody>
-                {Users()}
-            </tbody>
+           
+            {Users()}
             
+
+            
+        
         </table>
-        <div className='table_page'><Button className="table_page_previous" value="Previous" /> <Button className="table_page_button " value="1"/> <Button className='table_page_next' value="Next"/> </div>
+        <div className='table_page'><Button  className="table_page_previous" value="Previous" /> <Button  className="table_page_button " value="1"/> <Button className='table_page_next' value="Next"/> </div>
+        
         
     </div>
   )
