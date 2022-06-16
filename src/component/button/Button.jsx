8b8d55bb44d3e4f970708ba809b4780747f06b63
Boucher Alexis <82830827+Alexlemars@users.noreply.github.com>
2022-06-16 15:27:32 +0200

    Creating an array with a react component

--- src/component/button/Button.jsx
+++ src/component/button/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Button({type,className,onClick,value}) {
+export default function Button({type,className,onClick,value,defaultValue}) {
   return (
     <div>
-        <button type={type} className={className} onClick={onClick} value={value} >{value}</button>
+        <button type={type} className={className} onClick={onClick} defaultValue={defaultValue} value={value} >{value}</button>
     </div>
   )
 }
--- src/component/table/Table.jsx
+++ src/component/table/Table.jsx
@@ -1,135 +1,137 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
+import { useTable,useSortBy,useGlobalFilter,usePagination } from "react-table";
 
-import Input from '../input/Input'
+import Input from "../input/Input";
+import Button from '../button/Button';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
-import Button from '../button/Button';
+import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
 
+import { COLUMNS } from "../../data/columns";
 
 export default function Table() {
 
-    const [search,setSearch]= useState("")
-    
-    const employees = useSelector(state => state.employees.employees)
-    const [dataArray,setDataArray]= useState([])
+  const employees = useSelector((state) => state.employees.employees);
+  const [dataArray, setDataArray] = useState([]);
 
-    const [dataSorted, setDataSorted] = useState(false);
-    const [order, setOrder] = useState("ascending");
+  useEffect(() => {
+    setDataArray(employees);
+  }, [employees]);
 
-    const sorting = (col) => {
-      if (order === "ascending") {
-        const sorted = [...dataArray].sort((a, b) =>
-          a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
-        );
-        setDataArray(sorted);
-        setOrder("descending");
-        setDataSorted(true);
-      }
-      if (order === "descending") {
-        const sorted = [...dataArray].sort((a, b) =>
-          a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
-        );
-        setDataArray(sorted);
-        setOrder("ascending");
-        setDataSorted(false);
-      }
-    };
+  const iconDown = () => {
+    return (
+      <span className="icon-carret">
+        <FontAwesomeIcon className="icon-carret_down" icon={faCaretDown} />
+      </span>
+    );
+  };
+  const iconUp = () => {
+    return (
+      <span className="icon-carret">
+        <FontAwesomeIcon className="icon-carret_down" icon={faCaretUp} />
+      </span>
+    );
+  };
 
-  
+  const tableInstance = useTable({
+    columns: COLUMNS,
+    data: dataArray,
+  },useGlobalFilter,useSortBy,usePagination);
 
+  const { getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      page,
+      nextPage,
+      previousPage,
+      canPreviousPage,
+      canNextPage,
+      pageOptions,
+      gotoPage,
+      setPageSize,
+      state,
+      setGlobalFilter,
+      prepareRow } =
+    tableInstance;
 
-    useEffect(()=>{
-        setDataArray(employees)
-    },[employees])
-    
+    const {GlobalFilter,pageSize} = state
 
-
-
-    const icon = ()=>{
-        return <span className='icon-carret-up'><FontAwesomeIcon className={dataSorted ? 'icon-carret_down' : "icon-carret-up" } icon={faCaretDown}/></span>
+    const pagesNumber = (e)=>{
+      const pageNumber = e.target.value ? Number(e.target.value) -1 : 0
+      gotoPage(pageNumber)
     }
-
-    
-
-   
-     
-    
-
-    const Users = () => {
-        return dataArray.filter((element)=>(
-            element.firstName.toLowerCase().includes(search)||
-            element.lastName.toLowerCase().includes(search) ||
-            element.startDate.toLowerCase().includes(search) ||
-            element.department.toLowerCase().includes(search) ||
-            element.dateOfBirth.toLowerCase().includes(search) ||
-            element.street.toLowerCase().includes(search) ||
-            element.city.toLowerCase().includes(search) ||
-            element.state.toLowerCase().includes(search) ||
-            element.zipCode.toLowerCase().includes(search)
-
-
-        )).map(element =>(
-            
-            <tbody key={element.firstName}>
-            <tr>
-            <td>{element.firstName}</td>
-            <td>{element.lastName}</td>
-            <td>{element.startDate}</td>
-            <td>{element.department}</td>
-            <td>{element.dateOfBirth}</td>
-            <td>{element.street}</td>
-            <td>{element.city}</td>
-            <td>{element.state}</td>
-            <td>{element.zipCode}</td>
-        </tr>
-        </tbody>      
-        )) 
-         
-    }
-
 
   return (
-    <div className='table-container'>
-    <div className='table_flex'>
-    <div>
+    <div className="table-container">
+      <div className="table_flex">
+      <div>
         
     
-        Show <select>
-            <option>10</option>
-            <option>25</option>
-            <option>50</option>
-            <option>100</option>
+        Show <select value={pageSize} onChange={e=>setPageSize(Number(e.target.value))} >
+           {
+             [10,25,50,100].map(pageSize=>(
+               <option key={pageSize} value={pageSize}>
+                 {pageSize}
+               </option>
+             ))
+           }
+            
             </select> entries
     </div>
-    
-    <Input value={search}  onChange={(e) => setSearch(e.target.value.toLowerCase())} className="table_flex_search" label="Search : "/>
-    </div>
-        <table className='table_style'>
-            <thead>
-                <tr>
-                    <th onClick={()=> sorting("firstName")} >First Name {icon()}</th>
-                    <th onClick={()=> sorting("lastName")}>Last Name </th>
-                    <th onClick={()=> sorting("startDate")}>Start Date </th>
-                    <th onClick={()=> sorting("department")}>Departement </th>
-                    <th onClick={()=> sorting("dateOfBirth")}>Date of Birth </th>
-                    <th onClick={()=> sorting("street")}> Street </th>
-                    <th onClick={()=> sorting("city")}>City </th>
-                    <th onClick={()=> sorting("state")}>State </th>
-                    <th onClick={()=> sorting("zipCode")}>Zip Code</th>
-                </tr>
-            </thead>
-           
-            {Users()}
-            
+        <Input
+          value={GlobalFilter}
+          onChange={(e) => setGlobalFilter(e.target.value.toLowerCase())}
+          className="table_flex_search"
+          label="Search : "
+        />
+      </div>
+      <table {...getTableProps()} className="table_style">
+        <thead>
+          {
+            headerGroups.map((headerGroup)=>(
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {
+                  headerGroup.headers.map(column => (
+                      <th {...column.getHeaderProps(column.getSortByToggleProps())}>{column.render('Header')}
+                      <span> 
+                        {column.isSorted ? (column.isSortedDesc ? iconDown() : iconUp()) : ""}
+                      </span>
+                      </th>  
+                  ))
+                }
+              </tr>
+              
+            ))
+          }
+          
+          
+        </thead>
 
-            
+        <tbody {...getTableBodyProps()}>
+          {page.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <div className='table_page'>
+        <Button onClick={()=>previousPage()} disabled={!canPreviousPage}  className="table_page_previous" value="Previous" />
+
+        {
+          pageOptions.map(page => (
+            <Button onClick={e=>pagesNumber(e)}   className="table_page_button "  value={page + 1}/> ))
+        }
         
-        </table>
-        <div className='table_page'><Button  className="table_page_previous" value="Previous" /> <Button  className="table_page_button " value="1"/> <Button className='table_page_next' value="Next"/> </div>
-        
-        
+        <Button onClick={()=>nextPage()} disabled={!canNextPage} className='table_page_next' value="Next"/> </div>
     </div>
-  )
+    
+  );
 }
